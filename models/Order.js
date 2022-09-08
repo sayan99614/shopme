89725dcb7e670efee9fd5dc9268cc43848f6c655
dheeman pati <dheemanpati53@gmail.com>
2022-09-08 16:30:20 +0530

    added oder model

---
+++ models/Order.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema({
+  orderItems: [
+    {
+      name: { type: String, required: true },
+      image: { type: String, required: true },
+      price: { type: Number, required: true },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  User: { type: mongoose.Schema.Types.ObjectId, required: true },
+  shippingAddress: {
+    fullname: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    postal: { type: String, required: true },
+  },
+  paymentMethod: { type: String, required: true },
+  itemsPrice: { type: Number, required: true },
+  taxPrice: { type: Number, required: true },
+  totalPrice: { type: Number, required: true },
+  isPaid: { type: Boolean, required: true, default: false },
+  isDelivered: { type: Boolean, required: true, default: false },
+  paidAt: { type: Date },
+  deliveredAt: { type: Date },
+});
+
+const Order = mongoose.models.Order
+  ? mongoose.models.Order
+  : mongoose.model("Order", Schema);
+
+export default Order;
