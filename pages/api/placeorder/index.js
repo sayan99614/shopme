7fa91911b655c0f20262fbc4e0236333b29f3edd
dheeman pati <dheemanpati53@gmail.com>
2022-09-08 16:29:33 +0530

    implemented palaceorder api

---
+++ pages/api/placeorder/index.js
@@ -0,0 +1,23 @@
+import { getSession } from "next-auth/react";
+import Order from "../../../models/Order";
+import db from "../../../utils/db";
+
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+  if (!session) {
+    res.status(401).send({ message: "Unauthorized please login " });
+  }
+
+  const { user } = session;
+
+  console.log(user);
+  await db.connect();
+  const newOrder = new Order({
+    ...req.body,
+    User: user._id,
+  });
+
+  const order = await newOrder.save();
+
+  res.status(201).send(order);
+}
